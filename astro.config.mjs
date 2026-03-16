a1b80f85244bf1c6760be9ee761397eefa45b04f
Jasdeep Dhillon <jdps2867@gmail.com>
2026-03-16 05:05:04 -0400

    Import Fonts and Icons from astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
-import { defineConfig, envField } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, envField, fontProviders } from "astro/config";
+
+import icon from "astro-icon";
 
 export default defineConfig({
   server: {
     port: 4000,
   },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Tiny5",
+      cssVariable: "--font-tiny5",
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Jersey 10 Charted",
+      cssVariable: "--font-Jersey-10-Charted",
+    },
+  ],
 
   trailingSlash: "never",
 
@@ -22,4 +35,10 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [
+    icon({
+      iconDir: "src/assets/icons",
+    }),
+  ],
 });
